4f6dad85a89f76a8e5f1b220d97c70aed795b87f
apan <s71440@gmail.com>
2019-08-11 13:50:27 +0800

    first time to commit the project which is the scroll delegate project by the type2

--- hikingDemo_scroll&zoom/ViewController.swift
+++ hikingDemo_scroll&zoom/ViewController.swift
@@ -8,11 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet var scrollViewColection: [UIScrollView]!
+    @IBOutlet var imageColection: [UIImageView]!
+    @IBOutlet weak var mainScroll: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        pageControl.numberOfPages = 9
+        pageControl.currentPage = 0
+        for scrollView in scrollViewColection {
+            scrollView.delegate = self
+            scrollView.maximumZoomScale = 5
+            scrollView.minimumZoomScale = 1
+            scrollView.zoomScale = 1
+        }
+        mainScroll.delegate = self
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let currentPage = Int(scrollView.contentOffset.x / scrollView.frame.size.width)
+        pageControl.currentPage = currentPage
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        
+        return imageColection[pageControl.currentPage]
     }
 
 
